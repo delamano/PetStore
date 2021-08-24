e64c139d2489554ce3dd2af4ae2d75ffc1e78a48
delamano <delamano@live.com>
2021-08-24 13:22:43 -0300

    Completo - Script AlterarPet (Put)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -73,6 +73,25 @@ public class Pet {
 
     }
 
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("kira"))
+                .body("status", is ("sold"))
+        ;
+
+    }
+
 
 
 }
